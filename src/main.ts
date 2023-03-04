d2d9742c97c9aaa1ee8a7edab7f005769a7d4437
yin-chong <2991673081@qq.com>
2023-03-04 18:30:55 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
 
 import './assets/main.css'
 
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
---
+++ src/views/Index.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <el-button type="primary" size="default" @click="increment">{{ state.count }}</el-button>
+    <el-button type="success" @click="changeName">{{ fullName }}</el-button>
+</div>
+</template>
+
+<script setup lang="ts">
+// import { $ref } from "vue/macros";
+import { reactive, ref, computed } from 'vue';
+
+// let count = $ref(0)
+// function increment() {
+//     count++;
+// }
+interface State {
+    count: number
+}
+let state:State = reactive({count: 0});
+let firstName = ref<string>('张');
+let secondName = ref<string>('三');
+const fullName = computed<string>({
+    get() {
+        return `${firstName.value} ${secondName.value}`
+    },
+    set(newwVal) {
+        [firstName.value, secondName.value] = newwVal.split(' ')
+    }
+})
+function increment() {
+    state.count ++;
+}
+function changeName() {
+    fullName.value = '李 四'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
